59bc02b275dfb2a6b9050aa0d3b243f8b02dccb3
Phil Charles <estofi.uk@gmail.com>
2016-01-25 17:17:03 +0000

    Add support for .mgf peaklist files

---
+++ MSLIB/Format/MgfFile.js
@@ -0,0 +1,120 @@
+"use strict";
+
+if (typeof MSLIB == 'undefined') var MSLIB = {};
+if (typeof MSLIB.Format == 'undefined') MSLIB.Format = {};
+MSLIB.Format.MgfFile = function () {
+
+ var MgfFile = function(f) {
+  this.Reader                 = new MSLIB.Common.Reader(f,this);
+  this.Reader.onprogress = function(data) {
+   if (data.lengthComputable) {                                            
+    this.Progress = parseInt(((data.loaded/data.total)*100).toFixed(2));
+   }
+  }
+  this.Ready                  = true;
+  this.Progress               = 100;
+  this.Report                 = false;
+  this.FileType               = "mgf";
+  this.Scans                  = [];
+ };
+
+ var headerParse = /^([^=]+)=(.+)$/;
+ var pepmassParse = /^(\S+)(?:\s+(\S+))?/;
+ var chargeParse = /^(\d+)\+?/;
+ var mzIntPairParse = /^(\S+)\s+(\S+)$/;
+
+ MgfFile.prototype.load = function() {
+  MSLIB.Common.Starting.call(this);
+  this.LastError = this.Reader.readText(
+   function() {
+    var text = this.result.replace(/\r\n?/gm,"\n");
+    var entries = text.split("END IONS");
+    while (entries[entries.length-1].match(/^\s*$/)) entries.pop(); // remove trailing blank lines
+    entries.forEach(function(entry,i) {
+     var mgfEntryLines = entry.substr(entry.indexOf("BEGIN IONS")+10).split("\n");
+     var headers = {};
+     var startIndex = 0;
+     for (var j = startIndex; j < mgfEntryLines.length; j++) {
+      if (!mgfEntryLines[j].length) continue;
+      if (mgfEntryLines[j].indexOf("=") <= 0) {
+       startIndex = j;
+       break;
+      }
+      var hdr = headerParse.exec(mgfEntryLines[j]);
+      if (hdr) {
+       headers[hdr[1]] = hdr[2];
+      }
+      else {
+       console.log(mgfEntryLines[j]);
+      }
+     }
+     if (headers.TITLE && ("PEPMASS" in headers)) {
+      var mzs = [];
+      var ints = [];
+      for (var j = startIndex; j < mgfEntryLines.length; j++) {
+       if (!mgfEntryLines[j].length) continue;
+       var mzIntPair = mzIntPairParse.exec(mgfEntryLines[j]);
+       if (mzIntPair) {
+        mzs.push(+mzIntPair[1]);
+        ints.push(+mzIntPair[2]);
+       }
+       else {
+        console.log(mgfEntryLines[j]);
+       }
+      }
+      var scan = new MSLIB.Data.Scan();
+      scan.ScanNumber = i;
+      var pm_match = pepmassParse.exec(headers.PEPMASS);
+      if (pm_match[1]) {
+       scan.PrecursorMzs = [+pm_match[1]];
+       if (pm_match[2]) {
+        scan.PrecursorIntensities = [+pm_match[2]];
+       }
+       if (headers.CHARGE) {
+        scan.PrecursorCharges = [+chargeParse.exec(headers.CHARGE)[1]];
+       }
+       scan.Spectrum = new MSLIB.Data.Spectrum(mzs, ints);
+       scan.Internal.Headers = headers;
+       this.Parent.Scans[i] = {Scan: scan};
+      }
+      else {
+       console.log("Failed to parse Precursor mass from PEPMASS in "+i+":"+entry);
+       return;
+      }
+     }
+     else {
+      console.log("Failed to parse TITLE and PEPMASS from entry "+i+":"+entry);
+      return;
+     }
+    },this);
+    MSLIB.Common.Finished.call(this.Parent);
+   },
+   0
+  );
+ };
+
+ MgfFile.prototype.export = function() {
+  var out = "";
+  this.Scans.forEach(function(s) {
+   var scan = s.Scan;
+   out += "BEGIN IONS\n";
+   out += ("TITLE=" + scan.Internal.Headers.TITLE + "\n");
+   out += ("PEPMASS=" + scan.PrecursorMzs[0]);
+   if (scan.PrecursorIntensities.length) out += (" " + scan.PrecursorIntensities[0]);
+   out += "\n";
+   Object.keys(scan.Internal.Headers).forEach(function(key) {
+    if ((key != "TITLE") && (key != "PEPMASS")) {
+     out += (key + "=" + scan.Internal.Headers[key])+"\n";
+    }
+   });
+   scan.Spectrum.mzs.forEach(function(mz,i) {
+    out += (mz + " " + scan.Spectrum.ints[i] + "\n");
+   });
+   out += "END IONS\n";  
+  });
+  return out;
+ }
+
+ return MgfFile;
+
+}();
