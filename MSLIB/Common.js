4a79cb14dee4cfc80512fd8f6a00358a89715fdd
Phil Charles <estofi.uk@gmail.com>
2017-01-27 16:33:42 +0000

    Refactor MSLIB.Common.  Rename variables in line with lower-case first letter for functions only.  Throw Errors rather than printing to console.  Remove waitUntil (very inefficient) and replace with callAsync (for immediate async execution, using a Promise) and whenReady (attaches the function to object in question, will now be triggered by MSLIB.Common.finished). Expand core state functions with new MSLIB.Common.initialise and .progress.  Remove MSLIBWorker (WaitUntilPing event handler now unnecessary, Ready and Progress state of main thread host should be controlled by specific main thread message handling rather than changed via generic Ready and Progress messages from within worker).  Now create new Worker directly, generating uri with getMSLIBWorkerURI.

--- MSLIB/Common.js
+++ MSLIB/Common.js
@@ -1,95 +1,81 @@
 "use strict";
 
 if (typeof MSLIB == 'undefined') var MSLIB = {};
-MSLIB.Common = function _SOURCE(){
+MSLIB.Common = function _SOURCE() {
 
- var WaitStack = [];
  var waitUntil = function(tfunc,rfunc) {
-  WaitStack.push([tfunc,rfunc])
-  if (self.document === undefined) self.postMessage("WaitUntilPing");
-  else self.postMessage("WaitUntilEcho", "*");
+  throw new Error("Error: waitUntil has been removed.  Re-factor to use window.setTimeout or MSLIB.Common.whenReady");
  }
 
- self.addEventListener("message", function(e) {
-  if (e.data == "WaitUntilEcho") {
-   e.stopImmediatePropagation();
-   if (WaitStack.length > 0) {
-    var args = WaitStack.shift();
-    if (args[0]()) args[1]();
-    else {
-     WaitStack.push([args[0],args[1]])
-     if (self.document === undefined) self.postMessage("WaitUntilPing");
-     else self.postMessage("WaitUntilEcho", "*");
-    }
-   }
-  }
- });
+ var callAsync = function(Func) {
+  Promise.resolve().then(Func);
+ }
 
- if (!(self.File && self.FileReader && self.Blob)) console.log("Warning: Reader requires full File API support!");
+ if (!(self.File && self.FileReader && self.Blob)) throw new Error("Reader requires full File API support");
  
- var Reader = function(f,parent) {
-  var r = new FileReader();
-  if ((typeof(f) == 'object') && f.constructor === File) {
-   r.File = f;
+ var Reader = function(F,Parent) {
+  var R = new FileReader();
+  if ((typeof(F) == 'object') && F.constructor === File) {
+   R.File = F;
   }
   else {
    throw new Error("ReaderInvalidFileObject");
   }
-  r.Parent = parent;
-  r.Position = 0;
-  r.Report = false;
-  r.readBinary = readBinary;
-  r.readText = readText;
-  r.Progress = 0;
-  r.onprogress = function(e) { r.Progress = (e.lengthComputable ? (e.loaded/e.total)*100 : -1 ) };
-  r.onerror = (function(e) { console.log("Reader ("+this.File.name+"): Error - "+this.error.name)}).bind(r);
-  r.onabort = (function(e) { console.log("Reader ("+this.File.name+"): Aborted")}).bind(r);
-  return r;
+  R.Parent = Parent;
+  R.Position = 0;
+  R.Report = false;
+  R.readBinary = readBinary;
+  R.readText = readText;
+  R.Ready = true;
+  R.Progress = 100;
+  R.onprogress = function(e) { R.Progress = (e.lengthComputable ? (e.loaded/e.total)*100 : -1 ) };
+  R.onerror = (function(e) { console.log("Reader ("+this.File.name+"): Error - "+this.error.name)}).bind(R);
+  R.onabort = (function(e) { console.log("Reader ("+this.File.name+"): Aborted")}).bind(R);
+  return R;
  };
 
- var getFileSlice = function(pos,len) {
-  if (pos >= this.File.size) {
-   console.log("Error: Last valid file offset ("+(this.File.size-1)+") is before offset " + pos);
+ var getFileSlice = function(Pos,Len) {
+  if (Pos >= this.File.size) {
+   console.log("Error: Last valid file offset ("+(this.File.size-1)+") is before offset " + Pos);
    throw new Error("ReaderInvalidFileOffset");
   }
   else {
-   var fS;
-   if (len && (len < (this.File.size - pos))) {
-    fS = this.File.slice(pos, pos + len);
-    this.Position = pos + len;
+   var FS;
+   if (Len && (Len < (this.File.size - Pos))) {
+    FS = this.File.slice(Pos, Pos + Len);
+    this.Position = Pos + Len;
    }
    else {
-    fS = this.File.slice(pos);
+    FS = this.File.slice(Pos);
     this.Position = this.File.size;
    }
-   return(fS);
+   return(FS);
   }
  }
 
- var readBinary = function(callback,pos,len) {
-  return readAs.call(this,this.readAsArrayBuffer,callback,pos,len);
+ var readBinary = function(Callback,Pos,Len) {
+  return readAs.call(this,this.readAsArrayBuffer,Callback,Pos,Len);
  }
 
- var readText = function(callback,pos,len) {
-  return readAs.call(this,this.readAsText,callback,pos,len);
+ var readText = function(Callback,Pos,Len) {
+  return readAs.call(this,this.readAsText,Callback,Pos,Len);
  }
 
- var readAs = function(method,callback,pos,len) {
-  self.readAs=[pos,len,this.readyState];
+ var readAs = function(Method,Callback,Pos,Len) {
   if (this.readyState == 1) throw new Error("ReaderNotReady");
-  pos = pos > 0 ? pos : 0;
-  len = len > 0 ? len : 0;
+  Pos = Pos > 0 ? Pos : 0;
+  Len = Len > 0 ? Len : 0;
   if (this.Report) {
    if (this.LastReadStart) console.log("Reader ("+this.File.name+"): Current buffer is offsets "+this.LastReadStart+" to "+(this.LastReadEnd));
-   console.log("Reader ("+this.File.name+"): Requested offsets "+pos+" to "+(len ? (pos+len) : this.File.size));
+   console.log("Reader ("+this.File.name+"): Requested offsets "+Pos+" to "+(Len ? (Pos+Len) : this.File.size));
   }
-  if (this.LastReadStart && (pos >= this.LastReadStart) && ((pos + len) <= (this.LastReadEnd))) {
-   this.Position = pos + len;
-   var pseudopos = pos - this.LastReadStart;
+  if (this.LastReadStart && (Pos >= this.LastReadStart) && ((Pos + Len) <= (this.LastReadEnd))) {
+   this.Position = Pos + Len;
+   var Pseudopos = Pos - this.LastReadStart;
    //Return a pseudo reader element contaning result, parent and position.  Can't call reader methods on it though (would require duplication)
-   if (this.Report) console.log("Reader ("+this.File.name+"): Returning psuedo-offsets "+pseudopos+" to "+(pseudopos+len));
+   if (this.Report) console.log("Reader ("+this.File.name+"): Returning psuedo-offsets "+Pseudopos+" to "+(Pseudopos+Len));
    if (!this.Cache) this.Cache = this.result.slice(0); //speed up repeated access (Reader.result is slow to access)
-   waitUntil(() => true,callback.bind({result: this.Cache.slice(pseudopos, pseudopos + len), Parent: this.Parent, Position: this.Position}));
+   callAsync(Callback.bind({result: this.Cache.slice(Pseudopos, Pseudopos + Len), Parent: this.Parent, Position: this.Position}));
   }
   else {
    if (this.LastReadStart) {
@@ -97,14 +83,14 @@ MSLIB.Common = function _SOURCE(){
     delete this.LastReadEnd;
     delete this.Cache;
    }
-   var fS = getFileSlice.call(this,pos,len);
-   if (fS) {
-    if (this.Report) console.log("Reader ("+this.File.name+"): New read of offsets "+pos+" to "+this.Position);
-    this.LastReadStart = pos;
-    this.LastReadEnd = pos + len;
-    this.onloadend = callback.bind(this);
-    if (this.Report) console.log("Reader ("+this.File.name+"): Calling "+method.name);
-    method.call(this,fS);
+   var FS = getFileSlice.call(this,Pos,Len);
+   if (FS) {
+    if (this.Report) console.log("Reader ("+this.File.name+"): New read of offsets "+Pos+" to "+this.Position);
+    this.LastReadStart = Pos;
+    this.LastReadEnd = Pos + Len;
+    this.onloadend = Callback.bind(this);
+    if (this.Report) console.log("Reader ("+this.File.name+"): Calling "+Method.name);
+    Method.call(this,FS);
    }
    else {
     throw new Error("ReaderInvalidFileSlice");
@@ -112,58 +98,66 @@ MSLIB.Common = function _SOURCE(){
   }
  }
 
+ var initialise = function() {
+  this.Ready = true;
+  this.Progress = 100;
+  this.Report = false;
+ }
+
  var starting = function() {
   this.Ready = false;
   this.Progress = 0;
  }
 
+ var progress = function(p) {
+  this.Progress = p;
+ }
+
  var finished = function() {
   this.Ready = true;
   this.Progress = 100;
+  if (this.onReady) { 
+   var Func = this.onReady; 
+   delete(this.onReady);
+   Func();
+  }
+ }
+
+ var whenReady = function(Obj,Func) {
+  if (Obj.onReady) throw new Error("OnReadyFunctionExists");
+  if (Obj.Ready) Func();
+  else Obj.onReady = Func;
  }
 
  var getMSLIBWorkerURI = function(OnMessage,SelectCoreModules,ExtraModules,ExtraModuleNameSpaces) {
   return URL.createObjectURL(new Blob([
-   recursiveSOURCE([MSLIB],["MSLIB"],SelectCoreModules).concat(recursiveSOURCE(ExtraModules,ExtraModuleNameSpaces),
+   getRecursiveSOURCE([MSLIB],["MSLIB"],SelectCoreModules).concat(getRecursiveSOURCE(ExtraModules,ExtraModuleNameSpaces),
     "self.addEventListener(\"message\","+OnMessage.toString()+");"
    ).join(";\n")
   ]));
  }
 
- var recursiveSOURCE = function(oArr,pArr,cArr) {
-  if (!oArr) return [];
-  else return [].concat.apply([],oArr.map((o,i) => {
-   if (!o) return [];
-   var path = (pArr && pArr[i]);
-   if (!path) return [];
-   var declaration = (path.indexOf(".") < 0 ? "var " : "")+path+"=";
-   if (o._SOURCE) return declaration+o._SOURCE.toString()+"()";
-   else return [].concat.apply([declaration+"{}"],Object.keys(o).map((k) => (cArr && !cArr.find((e)=>(e==k))) ? [] : recursiveSOURCE([o[k]],[path+"."+k],cArr)));
+ var getRecursiveSOURCE = function(OArr,PArr,CArr) {
+  if (!OArr) return [];
+  else return [].concat.apply([],OArr.map((O,i) => {
+   if (!O) return [];
+   var Path = (PArr && PArr[i]);
+   if (!Path) return [];
+   var Declaration = (Path.indexOf(".") < 0 ? "var " : "")+Path+"=";
+   if (O._SOURCE) return Declaration+O._SOURCE.toString()+"()";
+   else return [].concat.apply([Declaration+"{}"],Object.keys(O).map((K) => (CArr && !CArr.find((E)=>(E==K))) ? [] : getRecursiveSOURCE([O[K]],[Path+"."+K],CArr)));
   }));
  }
 
- var MSLIBWorker = function(uri) {
-  var worker = new Worker(uri);
-  worker.addEventListener("message", function(e) {
-   if (e.data == "WaitUntilPing") {
-    e.stopImmediatePropagation();
-    worker.postMessage("WaitUntilEcho");
-   }
-   else if ((e.data[0] == "Ready") || (e.data[0] == "Progress")) {
-    e.stopImmediatePropagation();
-    worker[e.data[0]] = e.data[1]
-   }
-  });
-  return worker;
- }
-
  return {
-  waitUntil: waitUntil,
+  callAsync: callAsync,
   Reader: Reader,
+  initialise: initialise,
   starting: starting,
+  progress: progress,
   finished: finished,
+  whenReady: whenReady,
   getMSLIBWorkerURI : getMSLIBWorkerURI,
-  MSLIBWorker: MSLIBWorker,
   _SOURCE: _SOURCE
  }
 
